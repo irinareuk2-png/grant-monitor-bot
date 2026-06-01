0d7f203ec87504d418e168fa73ef8637c995043b
irinareuk2-png <irinareuk2@gmail.com>
2026-06-02 01:09:22 +0300

    Update getgrant.py

--- parsers/getgrant.py
+++ parsers/getgrant.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def get_grants():
@@ -21,7 +22,17 @@ def get_grants():
         text = title.get_text(" ", strip=True)
 
         if "access_time" in text:
-            print(text[:200])
+
+            grant_title = text.split("access_time")[0].strip()
+
+            if len(grant_title) > 15:
+                grants.append(grant_title)
+
+    grants = sorted(set(grants))
+
+    print("Гранти:")
+    for grant in grants[:20]:
+        print(grant)
 
     print(f"GETGRANT знайдено: {len(grants)}")
 
